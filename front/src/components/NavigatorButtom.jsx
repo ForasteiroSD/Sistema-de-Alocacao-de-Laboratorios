01fb915982571fc5f9dffef47ca5ed35af998ff8
Diogo Sobral <dsforasteiro@gmail.com>
2024-05-15 20:31:04 -0300

    Nav Buttons Fineshed

--- front/src/components/NavigatorButtom.jsx
+++ front/src/components/NavigatorButtom.jsx
@@ -1,21 +1,23 @@
 /* Packages */
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 /* Css */
 import './NavigatorButtom.css'
 
-export default function NavigatorButtom({icon, name, link, start}) {
+export default function NavigatorButtom({icon, name, link, id, isActive, setActive}) {
     return (
-    <div className="Wrapper">
-        {start ? <input type="radio" id={link} name='Url' defaultChecked /> : <input type="radio" id={link} name='Url' />}
-        <div className='flex NavigatorButtom'>
+    <div>
+        {isActive == id && (
+            <motion.div layoutId="SelectedMenuButton" transition={{type: 'spring', duration: .5}} className="SelectedMenuButton" />
+            // <motion.div layoutId="SelectedMenuButton" className="SelectedMenuButton" />
+        )}
+
+        <div className="flex NavigatorButtom" style={isActive == id ? {'color': '#22b1b1'} : null}>
             {icon}
-            <Link to={link} className="Link">
-            {/* <a href={link}> */}
-                <label htmlFor={link} name='Url'>
-                    {name}
-                </label>
-            {/* </a> */}
+
+            <Link to={link} className="Link" style={isActive == id ? {'color': '#22b1b1'} : null} onClick={() => setActive(id)}>
+                {name}
             </Link>
         </div>
     </div>
--- front/src/components/SideMenu.jsx
+++ front/src/components/SideMenu.jsx
@@ -8,8 +8,18 @@ import NavigatorButtom from './NavigatorButtom'
 
 /* Css */
 import './SideMenu.css'
+import { useState } from "react";
+
+/* Variables/Consts */
+const links = [
+    {id: 'mainpage', icon: <HiHome className="Icons" />, name: 'Página Inicial', link: '/'},
+    {id: 'labs', icon: <FaComputer className="Icons" />, name: 'Laboratorios', link: '/laboratorios'},
+    {id: 'reserves', icon: <PiNotepad className="Icons" />, name: 'Reservas', link: '/reservas'}
+]
 
 export default function SideMenu() {
+    const [activeLink, setActiveLink] = useState(links[0].id);
+
     return (
         <div className='SideMenu'>
             <div className='flex h'>
@@ -18,9 +28,9 @@ export default function SideMenu() {
             <hr />
 
             <nav className='flex c NavButtons'>
-                <NavigatorButtom icon={<HiHome className="Icons" />} name={'Página Inicial'} link={'/'} start={true}/>
-                <NavigatorButtom icon={<FaComputer className="Icons" />} name={'Laboratorios'} link={'/laboratorios'}/>
-                <NavigatorButtom icon={<PiNotepad className="Icons" />} name={'Reservas'} link={'/reservas'}/>
+                {links.map((link) => (
+                    <NavigatorButtom key={link.id} icon={link.icon} name={link.name} link={link.link} id={link.id} isActive={activeLink} setActive={setActiveLink} />
+                ))}
             </nav>
         </div>
     )
