bec722f23a678fa9e97529c51d5feee4a22c400d
Diogo Sobral <dsforasteiro@gmail.com>
2024-05-29 10:40:45 -0300

    Input component comments added
    Coments of the paramns of each type of input added

--- front/src/components/Input.jsx
+++ front/src/components/Input.jsx
@@ -12,6 +12,7 @@ export default function Input({type, label, placeholder, id, values, callback, f
         else input.type = 'text';
     }
 
+    //Params: type, placeholder(optional), id, values, required(optional)
     if(type == 'dropdown') {
         if(values) {
             return (
@@ -25,6 +26,7 @@ export default function Input({type, label, placeholder, id, values, callback, f
         }
     }
     
+    //Params: type, placeholder(optional), callback
     else if(type == 'submit') {
         return (
             <button className='Inputs Submit' type='submit' onClick={callback}>{placeholder}</button>
@@ -32,6 +34,7 @@ export default function Input({type, label, placeholder, id, values, callback, f
         )
     }
 
+    //Params: type, label(optional), placeholder(optional), id, maxDate(optional), required(optional)
     else if(type == 'date') {
         return (
             <div className='Inputs Input flex c'>
@@ -41,6 +44,7 @@ export default function Input({type, label, placeholder, id, values, callback, f
         )
     }
 
+    //Params: type, label(optional), placeholder(optional), id, formatter(optional), autoComplete(if type='password'), required(optional)
     else {
         return (
             <div className='Inputs Input flex c'>
