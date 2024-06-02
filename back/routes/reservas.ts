6a7a57e011ca8e7718aebee83050b832d4a15f18
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-06-02 18:20:18 -0300

    Envio de email na rota de criar reserva
    Ao criar uma reserva um email de notificação é enviado ao responsável pelo laboratório

--- back/routes/reservas.ts
+++ back/routes/reservas.ts
@@ -1,9 +1,8 @@
-import { Request, Response, Router } from 'express'
-import { PrismaClient, reserva } from '@prisma/client'
-import { dias_semana } from '../index';
+import { Request, Response, Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+import nodemailer from 'nodemailer';
+import { env } from 'node:process';
 
-const router = Router();
-const prisma = new PrismaClient();
 
 interface ResTeste {
     dia: Date;
@@ -18,6 +17,18 @@ interface ResIns {
     duracao: string;
 }
 
+const router = Router();
+const prisma = new PrismaClient();
+const dias_semana = [
+    'Domingo',
+    'Segunda',
+    'Terça',
+    'Quarta',
+    'Quinta',
+    'Sexta',
+    'Sábado'
+]
+
 function verificaConflito(inicio1: Number, fim1: Number, inicio2: Number, fim2: Number) {
 
     if (inicio1 >= inicio2 && inicio1 < fim2) return true;
@@ -26,6 +37,64 @@ function verificaConflito(inicio1: Number, fim1: Number, inicio2: Number, fim2: 
 
     return false;
 
+}
+
+function sendEmail(tipo: string, labName: string, userName: string, data_inicio: string, data_fim: string, hora_inicio: string, duracao: string,
+    horarios: any, email: string
+) {
+    const transport = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: env.EMAIL_USER,
+          pass: env.EMAIL_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    let text1 = `Nova reserva ${tipo} feita no laboratório ${labName}\n\nDados da Reserva:\nUsuário: ${userName}\n`;
+    let text2 = `<h1>Nova reserva ${tipo} feita no laboratório ${labName}</h1><h3>Dados da Reserva:</h3><p style='margin: 0'>Usuário: ${userName}</p>`
+
+    if(tipo === 'Única') {
+        text1 += `Data: ${data_inicio}\nHorário: ${hora_inicio}\nDuração: ${duracao}\n\n`;
+        text2 += `<div><p style='margin: 0'>Data: ${data_inicio}</p><p style='margin: 0'>Horário: ${hora_inicio}</p><p style='margin: 0'>Duração: ${duracao}</p></div>`;
+    } else if(tipo === 'Personalizada') {
+        for(const dia of horarios) {
+            text1 += `Data: ${dia.data.toISOString().split('T')[0]}\nHorário: ${dia.hora_inicio}\nDuração: ${dia.duracao}\n\n`;
+            text2 += `<div><p style='margin: 0'>Data: ${dia.data.toISOString().split('T')[0]}</p><p style='margin: 0'>Horário: ${dia.hora_inicio}</p style='margin: 0'><p>Duração: ${dia.duracao}</p></div><br><br>`;
+        }
+    } else if(tipo === 'Semanal'){
+        text1 += `Data Inicial: ${data_inicio}\nData Final: ${data_fim}\n\n`;
+        text2 += `<div><p style='margin: 0'>Data Inicial: ${data_inicio}</p><p style='margin: 0'>Data Final: ${data_fim}</p><br>`;
+        for(const dia of horarios) {
+            text1 += `Dia da semana: ${dia.dia_semana}\nHorário: ${dia.hora_inicio}\nDuração: ${dia.duracao}\n\n`;
+            text2 += `<div><p style='margin: 0'>Dia da semana: ${dia.dia_semana}</p><p style='margin: 0'>Horário: ${dia.hora_inicio}</p><p style='margin: 0'>Duração: ${dia.duracao}</p></div><br>`;
+        }
+        text2 += '</div>';
+    } else {
+        text1 += `Data Inicial: ${data_inicio}\nData Final: ${data_fim}\nHorário: ${hora_inicio}\nDuração: ${duracao}\n\n`;
+        text2 += `<p style='margin: 0'>Data Inicial: ${data_inicio}</p><p style='margin: 0'>Data Final: ${data_fim}</p><p style='margin: 0'>Horário: ${hora_inicio}</p><p style='margin: 0'>Duração: ${duracao}</p><br>`;
+    }
+
+    text1 += `\nAcesse ${env.PAGE_LINK} para ver mais!\n\n\nLABHUB - Alocação de Laboratórios`;
+    text2 += `<br><br><p style='margin: 0'>Acesse <a href="${env.PAGE_LINK}">Lab Hub</a> para ver mais!</p><br><br>LABHUB - Alocação de Laboratórios`;
+
+    const mailOptions = {
+        from: `"Lab Hub Reservas" <${env.EMAIL_USER}>`,
+        to: email,
+        subject: 'Lab Hub - Nova Reserva',
+        text: text1,
+        html: `<b>${text2}</b>`
+    };
+
+    transport.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log('Não foi possível enviar o email');
+        }
+    });
 }
 
 //Inserir reservas
@@ -59,9 +128,10 @@ function verificaConflito(inicio1: Number, fim1: Number, inicio2: Number, fim2: 
 //         duracao: "03:30"
 //     }
 // ]
+//Informar userName para envio no email e evitar busca pelo nome no banco
 router.post('/reserva', async (req: Request, res: Response) => {
 
-    const { userId, labName, tipo, data_inicio, data_fim, hora_inicio, duracao, horarios } = req.body;
+    const { userId, userName, labName, tipo, data_inicio, data_fim, hora_inicio, duracao, horarios } = req.body;
 
     let dataSearch1, dataSearch2;
 
@@ -107,7 +177,8 @@ router.post('/reserva', async (req: Request, res: Response) => {
                     include: {
                         dias: true
                     }
-                }
+                },
+                responsavel: true
             }
         });
 
@@ -273,17 +344,7 @@ router.post('/reserva', async (req: Request, res: Response) => {
             }
         });
 
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
-        //Enviar email
+        sendEmail(tipo, labName, userName, data_inicio, data_fim, hora_inicio, duracao, horarios, labReservas.responsavel.email);
 
         res.status(200).send('Reserva realizada');
         return;
