6888c48bd6d488dac41200187bf541fcb345719f
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-06-23 18:03:28 -0300

    Update files for backend host

--- back/index.ts
+++ back/index.ts
@@ -3,24 +3,26 @@ import cors from "cors";
 import user from "./routes/user";
 import labs from "./routes/labs";
 import reservas from "./routes/reservas";
+import { env } from "node:process";
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const whitelist = ['http://localhost:5173']; // lista das urls que podem acessar o back
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         if(!origin) {
-//             callback(new Error('No origin set on headers'))
-//         } else if (whitelist.indexOf(origin) !== -1) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }));
+const whitelist = [env.ALLOWED_LINKS]; // lista das urls que podem acessar o back
+app.use(cors({
+    origin: function (origin, callback) {
+        if(!origin) {
+            callback(new Error('No origin set on headers'))
+        } else if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}));
 
 export function stringData(data: Date, time: boolean) {
     if (!time) {
@@ -47,10 +49,16 @@ export function stringData(data: Date, time: boolean) {
     }
 }
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(PORT, () => {
     console.log("Server Online");
+});
+
+app.get('/', (req, res) => {
+    res.send("Vercel server");
 });
 
 app.use(user);
 app.use(labs);
 app.use(reservas);
+
+export default app;
