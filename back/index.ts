2c3c11b7d5bd4b6003e845b75dda75a23343e8fa
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-06-23 23:09:38 -0300

    Update allowed origins

--- back/index.ts
+++ back/index.ts
@@ -11,38 +11,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const whitelist = [env.ALLOWED_LINKS]; // lista das urls que podem acessar o back
 app.use(cors({
-    origin: function (origin, callback) {
-        if(!origin) {
-            callback(null, false);
-        } else if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(null, false);
-        }
-    }
+    origin: env.ALLOWED_LINKS ? [env.ALLOWED_LINKS] : '*'
 }));
 
 export function stringData(data: Date, time: boolean) {
     if (!time) {
         let string_aux1 = '';
-        if(data.getUTCDate() < 10) string_aux1 = '0'+(data.getUTCDate());
+        if (data.getUTCDate() < 10) string_aux1 = '0' + (data.getUTCDate());
         else string_aux1 += data.getUTCDate();
-        
+
         let string_aux2 = '';
-        if (data.getUTCMonth() < 10) string_aux2 = '0'+(data.getUTCMonth()+1);
-        else string_aux2 += (data.getUTCMonth()+1);
+        if (data.getUTCMonth() < 10) string_aux2 = '0' + (data.getUTCMonth() + 1);
+        else string_aux2 += (data.getUTCMonth() + 1);
 
         return `${string_aux1}/${string_aux2}/${data.getUTCFullYear()}`;
 
     } else {
         let string_hora = '';
-        if(data.getUTCHours() < 10) string_hora = '0' + data.getUTCHours();
+        if (data.getUTCHours() < 10) string_hora = '0' + data.getUTCHours();
         else string_hora += data.getUTCHours();
 
         let string_min = '';
-        if(data.getUTCMinutes() < 10) string_min = '0' + data.getUTCMinutes();
+        if (data.getUTCMinutes() < 10) string_min = '0' + data.getUTCMinutes();
         else string_min += data.getUTCMinutes();
 
         return `${string_hora}:${string_min}`;
