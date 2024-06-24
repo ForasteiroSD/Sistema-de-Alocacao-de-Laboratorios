a581613f74daa2fc5c2e86609b11a193475b16e8
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-06-23 21:54:24 -0300

    Create _redirects for deploy

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -41,9 +41,9 @@ function App() {
                                     {({ user }) => (
                                         user ? (
                                             <>
-                                                {!user.loading && <SideMenu />}
+                                                <SideMenu />
                                                 <div>
-                                                    {!user.loading && <Header />}
+                                                    <Header />
                                                     <Routes>
                                                         <Route path="/" element={<MainPage />} />
                                                         <Route path="/laboratorios" element={<Labs />} />
