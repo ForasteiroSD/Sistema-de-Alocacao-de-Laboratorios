2d688ed570850048b2fa3492aa2633a10097807b
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2024-06-23 23:27:38 -0300

    Testing allowed links

--- back/index.ts
+++ back/index.ts
@@ -10,6 +10,7 @@ const app = express();
 
 app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    console.log(env.ALLOWED_LINKS);
     res.header("Access-Control-Allow-Origin", env.ALLOWED_LINKS);
 	//Quais são os métodos que a conexão pode realizar na API
     res.header("Access-Control-Allow-Methods", 'GET,PATCH,POST,DELETE');
