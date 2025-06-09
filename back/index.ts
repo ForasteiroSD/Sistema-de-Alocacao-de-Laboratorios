7d1b9cf3ce4e7853984bf0fe59182c36209b09aa
Thiago Fernandes <86969851+fivvvve@users.noreply.github.com>
2025-06-09 00:50:14 -0300

    Update index.ts

--- back/index.ts
+++ back/index.ts
@@ -7,7 +7,7 @@ import { env } from "node:process";
 
 const PORT = env.PORT || 5000;
 const app = express();
-const whitelist = [env.ALLOWED_LINKS];
+// const whitelist = [env.ALLOWED_LINKS];
 // app.use(cors({
 //     origin: (origin, callback) => {
 //         if (!origin) {
@@ -21,43 +21,19 @@ const whitelist = [env.ALLOWED_LINKS];
 //         }
 //     }
 // }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '2mb'}));
+app.use(express.urlencoded({ extended: true, limit: "2mb", parameterLimit: 5000 }));
 
-export function stringData(data: Date, time: boolean) {
-    if (!time) {
-        let string_aux1 = '';
-        if (data.getUTCDate() < 10) string_aux1 = '0' + (data.getUTCDate());
-        else string_aux1 += data.getUTCDate();
-
-        let string_aux2 = '';
-        if (data.getUTCMonth() < 10) string_aux2 = '0' + (data.getUTCMonth() + 1);
-        else string_aux2 += (data.getUTCMonth() + 1);
-
-        return `${string_aux1}/${string_aux2}/${data.getUTCFullYear()}`;
-
-    } else {
-        let string_hora = '';
-        if (data.getUTCHours() < 10) string_hora = '0' + data.getUTCHours();
-        else string_hora += data.getUTCHours();
-
-        let string_min = '';
-        if (data.getUTCMinutes() < 10) string_min = '0' + data.getUTCMinutes();
-        else string_min += data.getUTCMinutes();
-
-        return `${string_hora}:${string_min}`;
-    }
-}
 
 app.listen(PORT, () => {
-    console.log("Server Online");
+    console.log("Server Online na porta " + PORT);
 });
 
 app.get('/', (req, res) => {
     res.send("Vercel server");
 });
 
-app.use(user);
+app.use("/user", user);
 app.use(labs);
 app.use(reservas);
 
